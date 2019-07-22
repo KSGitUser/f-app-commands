6b57729944a214ae32278938bc56bc6a7876824b
KSFirstUser <rusakk@mail.ru>
2019-07-23 00:39:33 +0300

    F-13 add files to commit

---
+++ src/components/User/UserProfile.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-layout container>
+    <v-flex>
+      <h1>Профиль пользователя новый</h1>
+      <v-list v-for="(user, key) in userData">
+        <p> {{ key }} : {{ user }} </p>
+      </v-list>
+    </v-flex>
+
+  </v-layout>
+
+</template>
+
+<script>
+  export default {
+    name: 'UserProfile',
+
+    data() {
+      return {
+        userData: {}
+      }
+
+    },
+
+    async created() {
+      /* this.$store.dispatch['users/fetchUser'];
+      const result = await this.$store.dispatch['users/fetchUser']; */
+      await this.$store.dispatch('fetchUser');
+      console.log(this.$store);
+      console.log(this.$store.getters.getUser);
+      this.userData = {
+        ...this.$store.getters.getUser
+      };
+    }
+
+  }
+
+</script>
+
+<style scoped>
+
+</style>
