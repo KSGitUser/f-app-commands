c3536ad0e24b178ca391681f7e77643c37496aa7
Marina Shvetckaia <missis_ol@mail.ru>
2019-07-24 04:25:28 +0300

    Settings updated

--- src/main.js
+++ src/main.js
@@ -19,13 +19,7 @@ new Vue({
     template: '<App/>',
     created () {
         firebase.initializeApp({
-            apiKey: 'AIzaSyBw--izaf_eLYb0rSrAkY4AReNFzTCyMD0',
-            authDomain: 'gb-project-dev.firebaseapp.com',
-            databaseURL: 'https://gb-project-dev.firebaseio.com',
-            projectId: 'gb-project-dev',
-            storageBucket: '',
-            messagingSenderId: '848679475804',
-            appId: '1:848679475804:web:dadc7d4a115af7da'
+            //..
         })
 
         firebase.auth().onAuthStateChanged(user => {
