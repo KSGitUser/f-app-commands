075c3517961720319c539ea466a9123a3cfafa73
KSFirstUser <rusakk@mail.ru>
2019-08-13 22:32:24 +0300

    Merge branch 'master' of https://github.com/DABronskikh/f-app-commands
    # Conflicts:
    #	.gitignore
    #	src/components/Auth/Login.vue
    #	src/router/index.js
    #	src/store/index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,72 +1,67 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from 'vue';
+import Router from 'vue-router';
 
-import Home from '../components/Home'
-import Page404 from '../components/Page/404'
+import Home from '../components/Home';
+import Page404 from '../components/Page/404';
 
 const Boards = resolve => {
-  require.ensure(['../components/Boards'], () => {
-    resolve(
-      require('../components/Boards')
-    )
-  })
-}
+    require.ensure(['../components/Boards'], () => {
+        resolve(require('../components/Boards'));
+    });
+};
 
 const Board = resolve => {
-  require.ensure(['../components/Board'], () => {
-    resolve(
-      require('../components/Board')
-    )
-  })
-}
+    require.ensure(['../components/Board'], () => {
+        resolve(require('../components/Board'));
+    });
+};
 
 const User = resolve => {
-  require.ensure(['../components/User'], () => {
-    resolve(
-      require('../components/User')
-    )
-  })
-}
+    require.ensure(['../components/User'], () => {
+        resolve(require('../components/User'));
+    });
+};
 
-Vue.use(Router)
+Vue.use(Router);
 
 const ifAuth = (to, from, next) => {
-  if (localStorage.getItem('user')) {
-    next()
-  } else {
-    next('/')
-  }
-}
+    if (localStorage.getItem('user')) {
+        next();
+    } else {
+        next('/');
+    }
+};
 
 export default new Router({
-  routes: [
-    {
-      path: '',
-      name: 'home',
-      component: Home,
-    },
-    {
-      path: '/boards',
-      name: 'boards',
-      component: Boards,
-      beforeEnter: ifAuth,
-    },{
-      path: '/board/:id',
-      name: 'board',
-      props: true,
-      component: Board,
-      beforeEnter: ifAuth,
-    },
-    {
-      path: '/user',
-      name: 'user',
-      component: User,
-      beforeEnter: ifAuth,
-    },
-    {
-      path: '*',
-      component: Page404,
-    }
-  ],
-  mode: 'history'
-})
+    routes: [
+        {
+            path: '',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/boards',
+            name: 'boards',
+            component: Boards,
+            beforeEnter: ifAuth
+        },
+        {
+            path: '/board/:id',
+            name: 'board',
+            props: true,
+            component: Board,
+            beforeEnter: ifAuth
+        },
+        {
+            path: '/user',
+            name: 'user',
+            component: User,
+            beforeEnter: ifAuth
+        },
+        {
+            path: '*',
+            component: Page404
+        }
+    ],
+    mode: 'history'
+});
